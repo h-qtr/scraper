2e3938dbf9cd542a785d974a56fbf8c12b182ffe
h-qtr <128894805+h-qtr@users.noreply.github.com>
2023-06-01 10:03:05 +0500

    app.py

--- app.py
+++ app.py
@@ -2,34 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import streamlit as st
 
-# Function to scrape the website
+@st.cache
 def scrape_website(url):
-    # Send a GET request to the URL
     response = requests.get(url)
-    # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
-    
-    # Extract the desired data
-    cars = []
-    car_elements = soup.find_all('div', class_='classified-listing')
-    for car_element in car_elements:
-        title = car_element.find('h3', class_='list-title').text.strip()
-        price = car_element.find('span', class_='price').text.strip()
-        cars.append({'Title': title, 'Price': price})
-    
-    return cars
+    return soup
 
-# Main code
+# Web scraping and displaying the data
 st.title("Web Data Scraper")
-url = "https://www.pakwheels.com/used-cars/family-cars/587667"
-cars = scrape_website(url)
+url = "https://www.jarir.com/qa-en/"
 
-# Display the scraped data using Streamlit
-if cars:
-    st.subheader("Scraped Car Data")
-    for car in cars:
-        st.write(f"Title: {car['Title']}")
-        st.write(f"Price: {car['Price']}")
-        st.write("---")
-else:
-    st.error("Failed to scrape data from the website.")
+# Scrape the website
+soup = scrape_website(url)
+
+# Find and display the desired data
+product_titles = soup.find_all('h2', class_='product-title')
+
+st.header("Product Titles")
+for title in product_titles:
+    st.write(title.get_text(strip=True))
