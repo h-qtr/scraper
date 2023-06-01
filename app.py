ad6a8fd83f8fc1a64031f68635c1b75c48eb3199
h-qtr <128894805+h-qtr@users.noreply.github.com>
2023-06-01 10:04:27 +0500

    app.py

--- app.py
+++ app.py
@@ -2,22 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import streamlit as st
 
-@st.cache
-def scrape_website(url):
+def scrape_data(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    return soup
 
-# Web scraping and displaying the data
-st.title("Web Data Scraper")
-url = "https://www.jarir.com/qa-en/"
+    # Extract relevant data from the webpage
+    title = soup.find('h1', class_='product-title').text.strip()
+    price = soup.find('span', class_='price-sales').text.strip()
+    description = soup.find('div', class_='product-details-description').text.strip()
 
-# Scrape the website
-soup = scrape_website(url)
+    return title, price, description
 
-# Find and display the desired data
-product_titles = soup.find_all('h2', class_='product-title')
+# Streamlit web app
+st.title('Web Data Scraper')
+url = st.text_input('Enter the URL to scrape')
 
-st.header("Product Titles")
-for title in product_titles:
-    st.write(title.get_text(strip=True))
+if st.button('Scrape'):
+    if url:
+        try:
+            title, price, description = scrape_data(url)
+            st.write('Title:', title)
+            st.write('Price:', price)
+            st.write('Description:', description)
+        except Exception as e:
+            st.write('Error occurred:', str(e))
+    else:
+        st.write('Please enter a URL to scrape.')
